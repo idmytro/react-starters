b06821e63c278a0c6b826f1b9084d568df1ee16a
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-19 20:54:24 +0100

    feat: next-js-pnpm-plus updates

--- s/next-js-pnpm-plus/eslint.config.mjs
+++ s/next-js-pnpm-plus/eslint.config.mjs
@@ -3,15 +3,18 @@ import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import packageJson from 'eslint-plugin-package-json'
 import stylistic from '@stylistic/eslint-plugin'
+import { includeIgnoreFile } from '@eslint/compat'
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
 const config = [
+  includeIgnoreFile(gitignorePath),
   ...compat.extends('next/core-web-vitals'),
   packageJson.configs.recommended,
   stylistic.configs.recommended,
