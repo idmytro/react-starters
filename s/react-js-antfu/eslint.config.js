767e674362c9c515de4c6a77f157f540be6edd2b
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2025-03-19 12:32:29 +0100

    2 starters

--- s/react-js-antfu/eslint.config.js
+++ s/react-js-antfu/eslint.config.js
@@ -1,5 +1,34 @@
 import antfu from '@antfu/eslint-config'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
-export default antfu({
-  react: true,
-})
+export default antfu(
+  {
+    react: true,
+  },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        1,
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
